4549a0b277f7de4781e5a904aaa0d7d4e8549401
Nicholas <64442818+iUseYahoo@users.noreply.github.com>
2022-01-27 13:44:58 +1100

    Create EncodeHexStringFromInt.cs

---
+++ EncodeHexStringFromInt.cs
@@ -0,0 +1,19 @@
+		internal static string EncodeHexStringFromInt(byte[] sArray, uint start, uint end)
+		{
+			string result = null;
+			if (sArray != null)
+			{
+				char[] array = new char[(end - start) * 2U];
+				uint num = end;
+				uint num2 = 0U;
+				while (num-- > start)
+				{
+					uint num3 = (uint)(sArray[(int)num] & 240) >> 4;
+					array[(int)num2++] = X509Utils.hexValues[(int)num3];
+					num3 = (uint)(sArray[(int)num] & 15);
+					array[(int)num2++] = X509Utils.hexValues[(int)num3];
+				}
+				result = new string(array);
+			}
+			return result;
+		}
