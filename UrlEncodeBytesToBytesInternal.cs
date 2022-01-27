18e9d7777eb423883cbce7959c4715a525289e08
Nicholas <64442818+iUseYahoo@users.noreply.github.com>
2022-01-27 13:47:28 +1100

    Create UrlEncodeBytesToBytesInternal.cs

---
+++ UrlEncodeBytesToBytesInternal.cs
@@ -0,0 +1,43 @@
+private static byte[] UrlEncodeBytesToBytesInternal(byte[] bytes, int offset, int count, bool alwaysCreateReturnValue)
+		{
+			int num = 0;
+			int num2 = 0;
+			for (int i = 0; i < count; i++)
+			{
+				char c = (char)bytes[offset + i];
+				if (c == ' ')
+				{
+					num++;
+				}
+				else if (!WebClient.IsSafe(c))
+				{
+					num2++;
+				}
+			}
+			if (!alwaysCreateReturnValue && num == 0 && num2 == 0)
+			{
+				return bytes;
+			}
+			byte[] array = new byte[count + num2 * 2];
+			int num3 = 0;
+			for (int j = 0; j < count; j++)
+			{
+				byte b = bytes[offset + j];
+				char c2 = (char)b;
+				if (WebClient.IsSafe(c2))
+				{
+					array[num3++] = b;
+				}
+				else if (c2 == ' ')
+				{
+					array[num3++] = 43;
+				}
+				else
+				{
+					array[num3++] = 37;
+					array[num3++] = (byte)WebClient.IntToHex(b >> 4 & 15);
+					array[num3++] = (byte)WebClient.IntToHex((int)(b & 15));
+				}
+			}
+			return array;
+		}
