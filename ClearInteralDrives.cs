160196512e4b1e598864447abd2bcc556598d291
Nicholas <64442818+iUseYahoo@users.noreply.github.com>
2022-01-27 13:30:17 +1100

    Update ClearInteralDrives.cs

--- ClearInteralDrives.cs
+++ ClearInteralDrives.cs
@@ -1,4 +1,7 @@
-		public static void ClearInternalDrives()
+// Here is some code from the Utility._internalDrives file
+private static string _internalDrives = string.Empty;
+
+public static void ClearInternalDrives()
 		{
 			Utility._internalDrives = string.Empty;
 		}
